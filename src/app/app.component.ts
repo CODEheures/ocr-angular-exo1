8fdd52efa8ace65fefbdaf9bb95bc64a1a1e9157
CODEheures <webmaster@codeheures.fr>
2018-08-30 23:19:19 +0200

    add PostListComponent & PostListItemComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ocr-angular-exo1';
+  posts: Post[] = [
+    {
+      title: 'Les gens méchants codent comme des glands',
+      content: 'Pour et mes aux criards rythmes, de tremblais genoux et ivre sourd, folle les de un des n\'auraient flache, des.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    { title: 'Les gens heureux codent mieux',
+      content: 'Déplore moqueur excitant vivre yeux la comme et voluptueux. Souris.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Les gens gentils codent bien aussi',
+      // tslint:disable-next-line:max-line-length
+      content: 'Amour gouvernail l\'aube des pas d\'hommes faisait amer des ou. Sentant navrantes noirs tohu-bohus tapages au leurs soleils flots revé, l\'éveil d\'ineffables regretter est illuminant.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ]
 }
